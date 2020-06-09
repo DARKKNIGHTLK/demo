d45b29e2113c8ac221a0fd6e83299c4c7d401617
DARKKNIGHTLK <1980242157@qq.com>
2020-06-09 17:32:16 +0800

    提交信息

--- src/main/java/com/javaboy/demo/DemoApplication.java
+++ src/main/java/com/javaboy/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("rrsssrdd");
+        System.out.println("rrssssssrdd");
         SpringApplication.run(DemoApplication.class, args);
     }
 
